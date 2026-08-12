4af193ffe08cf6d5508e31bcbea7d1ac316b1df4
Marcho2ias <dpdhruv70@gmail.com>
2026-08-12 18:42:11 +1200

    Made a Workable demo Borrowing/History page.

--- BorrowedBook.cs
+++ BorrowedBook.cs
@@ -8,6 +8,51 @@ namespace DustyCover
 {
     internal class BorrowedBook
     {
+        public string Title;
+        public string DueDate;
+        public string Status;       // "Active", "Overdue", or "Returned"
+        public string ReturnDate;
 
+        public BorrowedBook(string title, string dueDate, string status, string returnDate)
+        {
+            Title = title;
+            DueDate = dueDate;
+            Status = status;
+            ReturnDate = returnDate;
+        }
+
+        // Returns the text to show under the title
+        public string GetStatusText()
+        {
+            if (Status == "Active")
+            {
+                return "Due " + DueDate;
+            }
+            else if (Status == "Overdue")
+            {
+                return "Overdue - was due " + DueDate;
+            }
+            else
+            {
+                return "Returned " + ReturnDate;
+            }
+        }
+
+        // Returns the text to show on the action button
+        public string GetActionText()
+        {
+            if (Status == "Active")
+            {
+                return "Renew";
+            }
+            else if (Status == "Overdue")
+            {
+                return "Return";
+            }
+            else
+            {
+                return "Borrow again";
+            }
+        }
     }
 }
--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,21 +32,40 @@
             this.Allbtn = new System.Windows.Forms.Button();
             this.Activebtn = new System.Windows.Forms.Button();
             this.Duebtn = new System.Windows.Forms.Button();
-            this.Returnbtn = new System.Windows.Forms.Button();
+            this.Returnedbtn = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.Borrowbtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Status3 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Returnbtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Status2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Renewbtn = new System.Windows.Forms.Button();
+            this.Status1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Displaytext = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(30, 38);
+            this.label1.Location = new System.Drawing.Point(30, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(237, 39);
+            this.label1.Size = new System.Drawing.Size(229, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "My Borrowing";
             // 
@@ -59,6 +78,7 @@
             this.Allbtn.TabIndex = 1;
             this.Allbtn.Text = "All";
             this.Allbtn.UseVisualStyleBackColor = true;
+            this.Allbtn.Click += new System.EventHandler(this.Allbtn_Click);
             // 
             // Activebtn
             // 
@@ -69,6 +89,7 @@
             this.Activebtn.TabIndex = 2;
             this.Activebtn.Text = "Active";
             this.Activebtn.UseVisualStyleBackColor = true;
+            this.Activebtn.Click += new System.EventHandler(this.Activebtn_Click);
             // 
             // Duebtn
             // 
@@ -79,16 +100,18 @@
             this.Duebtn.TabIndex = 3;
             this.Duebtn.Text = "Due";
             this.Duebtn.UseVisualStyleBackColor = true;
+            this.Duebtn.Click += new System.EventHandler(this.Duebtn_Click);
             // 
-            // Returnbtn
+            // Returnedbtn
             // 
-            this.Returnbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Returnbtn.Location = new System.Drawing.Point(607, 101);
-            this.Returnbtn.Name = "Returnbtn";
-            this.Returnbtn.Size = new System.Drawing.Size(129, 37);
-            this.Returnbtn.TabIndex = 4;
-            this.Returnbtn.Text = "Returned";
-            this.Returnbtn.UseVisualStyleBackColor = true;
+            this.Returnedbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Returnedbtn.Location = new System.Drawing.Point(607, 101);
+            this.Returnedbtn.Name = "Returnedbtn";
+            this.Returnedbtn.Size = new System.Drawing.Size(146, 37);
+            this.Returnedbtn.TabIndex = 4;
+            this.Returnedbtn.Text = "Returned";
+            this.Returnedbtn.UseVisualStyleBackColor = true;
+            this.Returnedbtn.Click += new System.EventHandler(this.Returnedbtn_Click);
             // 
             // panel1
             // 
@@ -100,37 +123,173 @@
             this.panel1.Size = new System.Drawing.Size(794, 476);
             this.panel1.TabIndex = 5;
             // 
-            // groupBox1
+            // groupBox3
             // 
-            this.groupBox1.Location = new System.Drawing.Point(36, 40);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(704, 120);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
+            this.groupBox3.Controls.Add(this.pictureBox3);
+            this.groupBox3.Controls.Add(this.Borrowbtn);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.Status3);
+            this.groupBox3.Location = new System.Drawing.Point(36, 313);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(704, 142);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.pictureBox3.Image = global::DustyCover.Properties.Resources.TAOWCover;
+            this.pictureBox3.Location = new System.Drawing.Point(6, 13);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(96, 123);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // Borrowbtn
+            // 
+            this.Borrowbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Borrowbtn.Location = new System.Drawing.Point(525, 63);
+            this.Borrowbtn.Name = "Borrowbtn";
+            this.Borrowbtn.Size = new System.Drawing.Size(138, 36);
+            this.Borrowbtn.TabIndex = 4;
+            this.Borrowbtn.Text = "Borrow Again";
+            this.Borrowbtn.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(148, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(272, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "The Art Of Winning";
+            // 
+            // Status3
+            // 
+            this.Status3.Location = new System.Drawing.Point(148, 77);
+            this.Status3.Name = "Status3";
+            this.Status3.Size = new System.Drawing.Size(178, 22);
+            this.Status3.TabIndex = 2;
             // 
             // groupBox2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(36, 187);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.Returnbtn);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.Status2);
+            this.groupBox2.Location = new System.Drawing.Point(36, 166);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(704, 120);
+            this.groupBox2.Size = new System.Drawing.Size(704, 141);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             // 
-            // groupBox3
+            // pictureBox2
             // 
-            this.groupBox3.Location = new System.Drawing.Point(36, 324);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(704, 120);
-            this.groupBox3.TabIndex = 2;
-            this.groupBox3.TabStop = false;
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.pictureBox2.Image = global::DustyCover.Properties.Resources.CTBDCover;
+            this.pictureBox2.Location = new System.Drawing.Point(6, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(96, 123);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Returnbtn
+            // 
+            this.Returnbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Returnbtn.Location = new System.Drawing.Point(525, 61);
+            this.Returnbtn.Name = "Returnbtn";
+            this.Returnbtn.Size = new System.Drawing.Size(138, 36);
+            this.Returnbtn.TabIndex = 3;
+            this.Returnbtn.Text = "Return";
+            this.Returnbtn.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(141, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(346, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "The Courage To Be Disliked";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Status2
+            // 
+            this.Status2.Location = new System.Drawing.Point(148, 75);
+            this.Status2.Name = "Status2";
+            this.Status2.Size = new System.Drawing.Size(178, 22);
+            this.Status2.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.Renewbtn);
+            this.groupBox1.Controls.Add(this.Status1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(36, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(704, 141);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.pictureBox1.Image = global::DustyCover.Properties.Resources.AHCover;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Renewbtn
+            // 
+            this.Renewbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Renewbtn.Location = new System.Drawing.Point(525, 49);
+            this.Renewbtn.Name = "Renewbtn";
+            this.Renewbtn.Size = new System.Drawing.Size(138, 36);
+            this.Renewbtn.TabIndex = 2;
+            this.Renewbtn.Text = "Renew";
+            this.Renewbtn.UseVisualStyleBackColor = true;
+            // 
+            // Status1
+            // 
+            this.Status1.Location = new System.Drawing.Point(148, 63);
+            this.Status1.Name = "Status1";
+            this.Status1.Size = new System.Drawing.Size(178, 22);
+            this.Status1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(142, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 32);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Atomic Habits";
+            // 
+            // Displaytext
+            // 
+            this.Displaytext.Location = new System.Drawing.Point(37, 63);
+            this.Displaytext.Name = "Displaytext";
+            this.Displaytext.Size = new System.Drawing.Size(288, 22);
+            this.Displaytext.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 699);
+            this.ClientSize = new System.Drawing.Size(1087, 772);
+            this.Controls.Add(this.Displaytext);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.Returnbtn);
+            this.Controls.Add(this.Returnedbtn);
             this.Controls.Add(this.Duebtn);
             this.Controls.Add(this.Activebtn);
             this.Controls.Add(this.Allbtn);
@@ -139,6 +298,15 @@
             this.Text = "Borrowing/History";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,11 +318,24 @@
         private System.Windows.Forms.Button Allbtn;
         private System.Windows.Forms.Button Activebtn;
         private System.Windows.Forms.Button Duebtn;
-        private System.Windows.Forms.Button Returnbtn;
+        private System.Windows.Forms.Button Returnedbtn;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox Status1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Status3;
+        private System.Windows.Forms.TextBox Status2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Renewbtn;
+        private System.Windows.Forms.Button Borrowbtn;
+        private System.Windows.Forms.Button Returnbtn;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox Displaytext;
     }
 }
 
