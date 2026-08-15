7832dd1f7afe8d765ee82868c6f037dc7d9403f7
Gullu <dpdhruv70@gmail.com>
2026-08-15 18:25:49 +1200

    Revert "Created A Login Page."

--- Form1.cs
+++ Form1.cs
@@ -13,53 +13,9 @@ namespace DustyCover
 {
     public partial class Form1 : Form
     {
-
-        private UserController userController;
-
         public Form1()
         {
             InitializeComponent();
-            userController = new UserController();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            // Rounds the corners of lblBadge into a pill shape
-            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
-            int radius = 10;
-            Rectangle rect = lblBadge.ClientRectangle;
-            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
-            path.AddArc(rect.Right - radius, rect.Y, radius, radius, 270, 90);
-            path.AddArc(rect.Right - radius, rect.Bottom - radius, radius, radius, 0, 90);
-            path.AddArc(rect.X, rect.Bottom - radius, radius, radius, 90, 90);
-            path.CloseFigure();
-            lblBadge.Region = new Region(path);
-        }
-
-        private void btnLogin_Click(object sender, EventArgs e)
-        {
-            string email = txtEmail.Text;
-            string password = txtPassword.Text;
-
-            User matchedUser = userController.ValidateLogin(email, password);
-
-            if (matchedUser != null)
-            {
-                // Save the logged-in user so every other form can read it
-                CurrentUser.LoggedInUser = matchedUser;
-
-                lblError.Visible = false;
-
-                // Open the borrowing history page (Form2) and close the login window
-                Form2 borrowingForm = new Form2();
-                borrowingForm.Show();
-                this.Hide();
-            }
-            else
-            {
-                lblError.Text = "Incorrect email or password";
-                lblError.Visible = true;
-            }
         }
 
         private Point groupBox1OriginalLocation;
