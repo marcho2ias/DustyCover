c4816dc2fcd273ee09dd00cafd9c5cb7a63ba101
Marcho2ias <dpdhruv70@gmail.com>
2026-08-15 17:26:22 +1200

    Created A Login Page.

---
+++ CurrentUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DustyCover
+{
+    internal class CurrentUser
+    {
+        public static class CurrentUser
+        {
+            public static User LoggedInUser;
+
+            public static bool IsLoggedIn()
+            {
+                return LoggedInUser != null;
+            }
+
+            public static void Logout()
+            {
+                LoggedInUser = null;
+            }
+        }
+    }
+}
