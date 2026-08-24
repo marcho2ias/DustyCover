5c2c4d422ac96edb124cccdee939e0edaf676a39
Marcho2ias <dpdhruv70@gmail.com>
2026-08-24 17:18:59 +1200

    Made Sign-Up Page Panel Design

--- SignupForm.Designer.cs
+++ SignupForm.Designer.cs
@@ -28,21 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblQuote2 = new System.Windows.Forms.Label();
+            this.lblQuote1 = new System.Windows.Forms.Label();
+            this.lblQuote = new System.Windows.Forms.Label();
+            this.lblStoreName2 = new System.Windows.Forms.Label();
+            this.lblStoreName1 = new System.Windows.Forms.Label();
+            this.lblStoreName = new System.Windows.Forms.Label();
+            this.lblBadge = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(56)))), ((int)(((byte)(32)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblQuote2);
+            this.panel1.Controls.Add(this.lblQuote1);
+            this.panel1.Controls.Add(this.lblQuote);
+            this.panel1.Controls.Add(this.lblStoreName2);
+            this.panel1.Controls.Add(this.lblStoreName1);
+            this.panel1.Controls.Add(this.lblStoreName);
+            this.panel1.Controls.Add(this.lblBadge);
+            this.panel1.Location = new System.Drawing.Point(-4, -21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(391, 493);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(196)))), ((int)(((byte)(160)))));
+            this.label1.Location = new System.Drawing.Point(12, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(367, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "————————————————————————————————————————\r\n\r\n";
+            // 
+            // lblQuote2
+            // 
+            this.lblQuote2.AutoSize = true;
+            this.lblQuote2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuote2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(137)))), ((int)(((byte)(95)))));
+            this.lblQuote2.Location = new System.Drawing.Point(45, 445);
+            this.lblQuote2.Name = "lblQuote2";
+            this.lblQuote2.Size = new System.Drawing.Size(71, 20);
+            this.lblQuote2.TabIndex = 6;
+            this.lblQuote2.Text = "— Cicero";
+            // 
+            // lblQuote1
+            // 
+            this.lblQuote1.AutoSize = true;
+            this.lblQuote1.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuote1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(196)))), ((int)(((byte)(160)))));
+            this.lblQuote1.Location = new System.Drawing.Point(40, 407);
+            this.lblQuote1.Name = "lblQuote1";
+            this.lblQuote1.Size = new System.Drawing.Size(317, 27);
+            this.lblQuote1.TabIndex = 5;
+            this.lblQuote1.Text = "is like a body without a soul.\"";
+            // 
+            // lblQuote
+            // 
+            this.lblQuote.AutoSize = true;
+            this.lblQuote.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuote.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(196)))), ((int)(((byte)(160)))));
+            this.lblQuote.Location = new System.Drawing.Point(40, 380);
+            this.lblQuote.Name = "lblQuote";
+            this.lblQuote.Size = new System.Drawing.Size(249, 27);
+            this.lblQuote.TabIndex = 4;
+            this.lblQuote.Text = "\"A room without books";
+            // 
+            // lblStoreName2
+            // 
+            this.lblStoreName2.AutoSize = true;
+            this.lblStoreName2.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStoreName2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(230)))));
+            this.lblStoreName2.Location = new System.Drawing.Point(41, 207);
+            this.lblStoreName2.Name = "lblStoreName2";
+            this.lblStoreName2.Size = new System.Drawing.Size(99, 32);
+            this.lblStoreName2.TabIndex = 3;
+            this.lblStoreName2.Text = "Cover";
+            // 
+            // lblStoreName1
+            // 
+            this.lblStoreName1.AutoSize = true;
+            this.lblStoreName1.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStoreName1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(230)))));
+            this.lblStoreName1.Location = new System.Drawing.Point(39, 165);
+            this.lblStoreName1.Name = "lblStoreName1";
+            this.lblStoreName1.Size = new System.Drawing.Size(97, 32);
+            this.lblStoreName1.TabIndex = 2;
+            this.lblStoreName1.Text = "Dusty";
+            // 
+            // lblStoreName
+            // 
+            this.lblStoreName.AutoSize = true;
+            this.lblStoreName.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStoreName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(230)))));
+            this.lblStoreName.Location = new System.Drawing.Point(41, 91);
+            this.lblStoreName.Name = "lblStoreName";
+            this.lblStoreName.Size = new System.Drawing.Size(75, 64);
+            this.lblStoreName.TabIndex = 1;
+            this.lblStoreName.Text = "\r\nThe ";
+            // 
+            // lblBadge
+            // 
+            this.lblBadge.AutoSize = true;
+            this.lblBadge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(56)))), ((int)(((byte)(32)))));
+            this.lblBadge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBadge.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(196)))), ((int)(((byte)(160)))));
+            this.lblBadge.Location = new System.Drawing.Point(45, 36);
+            this.lblBadge.Name = "lblBadge";
+            this.lblBadge.Padding = new System.Windows.Forms.Padding(8, 3, 8, 3);
+            this.lblBadge.Size = new System.Drawing.Size(117, 31);
+            this.lblBadge.TabIndex = 0;
+            this.lblBadge.Text = "EST.2026";
             // 
             // SignupForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
             this.Name = "SignupForm";
             this.Text = "SignupForm";
             this.Load += new System.EventHandler(this.SignupForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblQuote2;
+        private System.Windows.Forms.Label lblQuote1;
+        private System.Windows.Forms.Label lblQuote;
+        private System.Windows.Forms.Label lblStoreName2;
+        private System.Windows.Forms.Label lblStoreName1;
+        private System.Windows.Forms.Label lblStoreName;
+        private System.Windows.Forms.Label lblBadge;
     }
 }
 
