5c181feb2428599965e8dccaa39db7c0187d52f5
Asanthika Imbulpitiya <AsanthikaI@op.ac.nz>
2026-08-27 17:26:51 +1200

    Dhruv: Merged the LoginForm to Master manually

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace DustyCover
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblQuote2 = new System.Windows.Forms.Label();
+            this.lblQuote1 = new System.Windows.Forms.Label();
+            this.lblQuote = new System.Windows.Forms.Label();
+            this.lblStoreName2 = new System.Windows.Forms.Label();
+            this.lblStoreName1 = new System.Windows.Forms.Label();
+            this.lblStoreName = new System.Windows.Forms.Label();
+            this.lblBadge = new System.Windows.Forms.Label();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.lblManage = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblFP = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.lblSignIn = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(56)))), ((int)(((byte)(32)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblQuote2);
+            this.panel1.Controls.Add(this.lblQuote1);
+            this.panel1.Controls.Add(this.lblQuote);
+            this.panel1.Controls.Add(this.lblStoreName2);
+            this.panel1.Controls.Add(this.lblStoreName1);
+            this.panel1.Controls.Add(this.lblStoreName);
+            this.panel1.Controls.Add(this.lblBadge);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(396, 493);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(196)))), ((int)(((byte)(160)))));
+            this.label1.Location = new System.Drawing.Point(12, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(367, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "————————————————————————————————————————\r\n\r\n";
+            // 
+            // lblQuote2
+            // 
+            this.lblQuote2.AutoSize = true;
+            this.lblQuote2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuote2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(137)))), ((int)(((byte)(95)))));
+            this.lblQuote2.Location = new System.Drawing.Point(45, 445);
+            this.lblQuote2.Name = "lblQuote2";
+            this.lblQuote2.Size = new System.Drawing.Size(71, 20);
+            this.lblQuote2.TabIndex = 6;
+            this.lblQuote2.Text = "— Cicero";
+            // 
+            // lblQuote1
+            // 
+            this.lblQuote1.AutoSize = true;
+            this.lblQuote1.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuote1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(196)))), ((int)(((byte)(160)))));
+            this.lblQuote1.Location = new System.Drawing.Point(40, 407);
+            this.lblQuote1.Name = "lblQuote1";
+            this.lblQuote1.Size = new System.Drawing.Size(317, 27);
+            this.lblQuote1.TabIndex = 5;
+            this.lblQuote1.Text = "is like a body without a soul.\"";
+            // 
+            // lblQuote
+            // 
+            this.lblQuote.AutoSize = true;
+            this.lblQuote.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuote.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(196)))), ((int)(((byte)(160)))));
+            this.lblQuote.Location = new System.Drawing.Point(40, 380);
+            this.lblQuote.Name = "lblQuote";
+            this.lblQuote.Size = new System.Drawing.Size(249, 27);
+            this.lblQuote.TabIndex = 4;
+            this.lblQuote.Text = "\"A room without books";
+            // 
+            // lblStoreName2
+            // 
+            this.lblStoreName2.AutoSize = true;
+            this.lblStoreName2.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStoreName2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(230)))));
+            this.lblStoreName2.Location = new System.Drawing.Point(41, 207);
+            this.lblStoreName2.Name = "lblStoreName2";
+            this.lblStoreName2.Size = new System.Drawing.Size(99, 32);
+            this.lblStoreName2.TabIndex = 3;
+            this.lblStoreName2.Text = "Cover";
+            // 
+            // lblStoreName1
+            // 
+            this.lblStoreName1.AutoSize = true;
+            this.lblStoreName1.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStoreName1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(230)))));
+            this.lblStoreName1.Location = new System.Drawing.Point(39, 165);
+            this.lblStoreName1.Name = "lblStoreName1";
+            this.lblStoreName1.Size = new System.Drawing.Size(97, 32);
+            this.lblStoreName1.TabIndex = 2;
+            this.lblStoreName1.Text = "Dusty";
+            // 
+            // lblStoreName
+            // 
+            this.lblStoreName.AutoSize = true;
+            this.lblStoreName.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStoreName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(230)))));
+            this.lblStoreName.Location = new System.Drawing.Point(41, 91);
+            this.lblStoreName.Name = "lblStoreName";
+            this.lblStoreName.Size = new System.Drawing.Size(75, 64);
+            this.lblStoreName.TabIndex = 1;
+            this.lblStoreName.Text = "\r\nThe ";
+            // 
+            // lblBadge
+            // 
+            this.lblBadge.AutoSize = true;
+            this.lblBadge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(56)))), ((int)(((byte)(32)))));
+            this.lblBadge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBadge.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(196)))), ((int)(((byte)(160)))));
+            this.lblBadge.Location = new System.Drawing.Point(45, 36);
+            this.lblBadge.Name = "lblBadge";
+            this.lblBadge.Padding = new System.Windows.Forms.Padding(8, 3, 8, 3);
+            this.lblBadge.Size = new System.Drawing.Size(117, 31);
+            this.lblBadge.TabIndex = 0;
+            this.lblBadge.Text = "EST.2026";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(48)))), ((int)(((byte)(24)))));
+            this.lblWelcome.Location = new System.Drawing.Point(451, 36);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(197, 32);
+            this.lblWelcome.TabIndex = 1;
+            this.lblWelcome.Text = "Welcome Back";
+            this.lblWelcome.Click += new System.EventHandler(this.lblWelcome_Click);
+            // 
+            // lblManage
+            // 
+            this.lblManage.AutoSize = true;
+            this.lblManage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblManage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(138)))), ((int)(((byte)(107)))));
+            this.lblManage.Location = new System.Drawing.Point(457, 72);
+            this.lblManage.Name = "lblManage";
+            this.lblManage.Size = new System.Drawing.Size(253, 20);
+            this.lblManage.TabIndex = 2;
+            this.lblManage.Text = "Log in to manage your borrowing";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(68)))), ((int)(((byte)(35)))));
+            this.lblEmail.Location = new System.Drawing.Point(462, 150);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(63, 24);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmail.Location = new System.Drawing.Point(461, 197);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(287, 32);
+            this.txtEmail.TabIndex = 4;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            this.txtEmail.Enter += new System.EventHandler(this.txtEmail_Enter);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(68)))), ((int)(((byte)(35)))));
+            this.lblPassword.Location = new System.Drawing.Point(461, 249);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(95, 24);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Location = new System.Drawing.Point(465, 287);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(287, 32);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassword_Enter);
+            // 
+            // lblFP
+            // 
+            this.lblFP.AutoSize = true;
+            this.lblFP.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(94)))), ((int)(((byte)(60)))));
+            this.lblFP.Location = new System.Drawing.Point(626, 335);
+            this.lblFP.Name = "lblFP";
+            this.lblFP.Size = new System.Drawing.Size(126, 18);
+            this.lblFP.TabIndex = 7;
+            this.lblFP.Text = "Forgot Password?";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(56)))), ((int)(((byte)(32)))));
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(239)))), ((int)(((byte)(230)))));
+            this.btnLogin.Location = new System.Drawing.Point(461, 367);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(287, 40);
+            this.btnLogin.TabIndex = 8;
+            this.btnLogin.Text = "Log In";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(58)))), ((int)(((byte)(42)))));
+            this.lblError.Location = new System.Drawing.Point(462, 113);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(281, 20);
+            this.lblError.TabIndex = 9;
+            this.lblError.Text = "Incorrect email or password !!";
+            this.lblError.Visible = false;
+            // 
+            // lblSignIn
+            // 
+            this.lblSignIn.AutoSize = true;
+            this.lblSignIn.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSignIn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(68)))), ((int)(((byte)(35)))));
+            this.lblSignIn.Location = new System.Drawing.Point(472, 435);
+            this.lblSignIn.Name = "lblSignIn";
+            this.lblSignIn.Size = new System.Drawing.Size(271, 20);
+            this.lblSignIn.TabIndex = 10;
+            this.lblSignIn.Text = "New here? Create an account";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(228)))), ((int)(((byte)(210)))));
+            this.ClientSize = new System.Drawing.Size(882, 493);
+            this.Controls.Add(this.lblSignIn);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblFP);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblManage);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.panel1);
+            this.Name = "LoginForm";
+            this.Text = "Login Page";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblBadge;
+        private System.Windows.Forms.Label lblStoreName;
+        private System.Windows.Forms.Label lblStoreName2;
+        private System.Windows.Forms.Label lblStoreName1;
+        private System.Windows.Forms.Label lblQuote2;
+        private System.Windows.Forms.Label lblQuote1;
+        private System.Windows.Forms.Label lblQuote;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Label lblManage;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblFP;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label lblSignIn;
+    }
+}
+
---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DustyCover
+{
+    internal class User
+    {
+        public string Email;
+        public string Password;
+        public string DisplayName;
+
+        public User(string email, string password, string displayName)
+        {
+            Email = email;
+            Password = password;
+            DisplayName = displayName;
+        }
+    }
+}
