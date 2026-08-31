fd81f1a325267f792fcd34be604ca00291f3e856
Marcho2ias <dpdhruv70@gmail.com>
2026-08-31 16:05:47 +1200

    Added Login Page.

---
+++ User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DustyCover
+{
+    internal class User
+    {
+       public string name Firstname;
+
+        public string name Email;
+
+        public string name Password;
+
+            public string User( string Firstname ,string password, string email )
+        {
+            this.
+        }
+        
+    }
+}
