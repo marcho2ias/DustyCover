bdcecc3d09dcebcba4057f5b47928c7a88b6df93
Asanthika Imbulpitiya <AsanthikaI@op.ac.nz>
2026-09-01 11:09:13 +1200

    Asa: The markers were in Program.cs from Master

--- Program.cs
+++ Program.cs
@@ -13,12 +13,9 @@ namespace DustyCover
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
-            Application.Run(new WelcomePage());
-=======
 
-            Application.Run(new UserAccount("TestUser"));
->>>>>>> UserAccount
+            Application.Run(new WelcomePage());
+
         }
     }
 }
